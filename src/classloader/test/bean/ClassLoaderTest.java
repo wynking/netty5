a6dbdd81e9a846e01081daf1fc79748cfe1dfaca
wynking <wyn409669098@126.com>
2018-11-27 16:54:07 +0800

    分支合并

--- src/classloader/test/bean/ClassLoaderTest.java
+++ src/classloader/test/bean/ClassLoaderTest.java
@@ -4,7 +4,7 @@ public class ClassLoaderTest {
 
     public static void main(String[] args) {  
         try {  
-          /*  //查看当前系统类路径中包含的路径条目  
+          /*  //查看当前系统类路径中包含的路径条目  我是分支。。。。
             System.out.println(System.getProperty("java.class.path"));  
             //调用加载当前类的类加载器（这里即为系统类加载器）加载TestBean  
             Class typeLoaded = Class.forName("classloader.test.bean.TestBean");  
